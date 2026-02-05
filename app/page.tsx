72fca819b75d67d7aba0ef3a3786fd88328b437e
davi-pc <davimatosc@hotmail.com>
2026-02-05 19:05:28 -0300

    cleaning

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import DashboardStats from "@/components/dashboard";
-import RecentStudents from "@/components/RecentStudents";
-import { TodaySchedule } from "@/components/TodaySchedules";
 import DashboardPage from "app/dashboard/page";
 
 export default function page() {
